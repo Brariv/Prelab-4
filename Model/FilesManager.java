ce7aa0420f862f354964a8e85a098afccca73e25
Brariv <119123309+Brariv@users.noreply.github.com>
2023-11-03 07:42:11 -0600

    organization

--- Model/FilesManager.java
+++ Model/FilesManager.java
@@ -8,25 +8,6 @@ import org.json.simple.parser.ParseException;
 
 public class FilesManager {
 
-    public static ArrayList<ArrayList<String>> readFilesCSV(String path) {
-        ArrayList<ArrayList<String>> newT = new ArrayList<ArrayList<String>>();
-        
-        try (BufferedReader reader = new BufferedReader(new FileReader(path))) {
-            String line;
-            while ((line = reader.readLine()) != null) {
-                String[] splitWords = line.split(",");
-                ArrayList<String> list = new ArrayList<String>();
-                for (String word : splitWords) {
-                    list.add(word.trim());
-                }
-                newT.add(list);
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        
-        return newT;
-    }
 
     public static void writeFilesCSV(String path, ArrayList<String> newT) {
         try (BufferedWriter writer = new BufferedWriter(new FileWriter(path))) {
@@ -36,20 +17,6 @@ public class FilesManager {
             }
         } catch (IOException e) {
             e.printStackTrace();
-        }
-    }
-
-    public static void appendlineCSV(String path, ArrayList<String> newT){
-        try {
-            FileWriter fw = new FileWriter(path, true);
-            for (String word : newT) {
-                fw.write(word);
-                fw.write(",");
-            }
-            fw.write("\n");
-            fw.close();
-        } catch (IOException e) {
-            System.out.println(e.getMessage());
         }
     }
 
@@ -82,6 +49,11 @@ public class FilesManager {
 
 
     }
+
+    public static void createCSVfile(){
+
+    }
+    
 
 
 
